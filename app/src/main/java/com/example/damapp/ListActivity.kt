e70cc66ae8e75e69328c0e9a66094bb873337396
Nageen Imran <imrannageen57@gmail.com>
2021-12-08 03:17:50 +0500

    add activty

--- app/src/main/java/com/example/damapp/ListActivity.kt
+++ app/src/main/java/com/example/damapp/ListActivity.kt
@@ -1,11 +1,30 @@
 package com.example.damapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListActivity : AppCompatActivity() {
+
+    private lateinit var float_btn:FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+        connectXML()
     }
+
+    private fun connectXML() {
+        float_btn=findViewById(R.id.floating_btn)
+        float_btn.setOnClickListener {
+            add_activity_btn()
+        }
+    }
+
+    private fun add_activity_btn() {
+        startActivity(Intent(this,AddActivity::class.java))
+    }
+
+
 }
